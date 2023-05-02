70a57f432df8dd8dbf82c561e9b5644dcaa50af4
5harath <19461590+5harath@users.noreply.github.com>
2023-05-02 15:53:06 +0530

    add files

---
+++ src/components/NoteList.js
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import Note from "./Note";
+import React from 'react';
+
+const styles = {
+    ul: {
+        listStyle: "none",
+        margin: 0,
+        padding: 0
+    }
+};
+
+function NoteList(props) {
+    return (
+        <ul style={styles.ul}>
+            {props.notes.map((note, index) => {
+                return <Note key={note.id} note={note} index={index} onChange={props.onToggle}/>
+            })}
+        </ul>
+    )
+}
+
+NoteList.propTypes = {
+    notes: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onToggle: PropTypes.func.isRequired
+};
+
+export default NoteList;
